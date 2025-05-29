9d9722aff8f700e56a6d700e2e5571d6f2ad66cf
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-05-29 19:49:20 +0900

    update : コンテンツ作成。

--- src/components/section/Me/Me.svelte
+++ src/components/section/Me/Me.svelte
@@ -1,9 +1,16 @@
-<script>
+<script lang="ts">
 </script>
 
 <section class="section-me section-style-base">
   <div class="section-me-contents">
-    <h1 class="perSection-title-base">について</h1>
+    <div class="perSection-title perSection-title-base">
+      <div class="blink-items">
+        <span>_</span><span>_</span><span>_</span><span>_</span><span>_</span>
+        <span>_</span><span>_</span><span>_</span><span>_</span><span>_</span>
+        <span>_</span>
+      </div>
+      について
+    </div>
   </div>
   <div class="about-me-block">
     <p>ウエハラリュウノスケ。二〇〇一年</p>
@@ -20,13 +27,78 @@
   .section-me {
     background-color: var(--turquoise-fresh);
   }
+
   .section-me-contents {
     width: 1280px;
   }
+
   @media (max-width: 768px) {
     .section-me-contents {
       width: 100%;
-      padding: 0 1.5rem 2.5rem;
+      padding-bottom: 2.5rem;
+    }
+
+    .perSection-title {
+      display: flex;
+      align-items: flex-end;
+      gap: 10px;
+    }
+
+    .blink-items {
+      position: relative;
+      top: 2px;
+    }
+
+    @keyframes blink {
+      0%,
+      100% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0;
+      }
+    }
+
+    .blink-items span {
+      display: inline-block;
+      animation: blink 1s infinite;
+    }
+
+    .blink-items span:nth-child(1) {
+      animation-delay: 0s;
+    }
+    .blink-items span:nth-child(2) {
+      animation-delay: 0.2s;
+    }
+    .blink-items span:nth-child(3) {
+      animation-delay: 0.4s;
+    }
+    .blink-items span:nth-child(4) {
+      animation-delay: 0.1s;
+    }
+    .blink-items span:nth-child(5) {
+      animation-delay: 0.3s;
+    }
+    .blink-items span:nth-child(6) {
+      animation-delay: 0.5s;
+    }
+    .blink-items span:nth-child(7) {
+      animation-delay: 0.15s;
+    }
+    .blink-items span:nth-child(8) {
+      animation-delay: 0.35s;
+    }
+    .blink-items span:nth-child(9) {
+      animation-delay: 0.25s;
+    }
+    .blink-items span:nth-child(10) {
+      animation-delay: 0.05s;
+    }
+    .blink-items span:nth-child(11) {
+      animation-delay: 0.45s;
+    }
+    .blink-items span:nth-child(12) {
+      animation-delay: 0.3s;
     }
   }
 </style>
