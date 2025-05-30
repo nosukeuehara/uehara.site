5e088994210546068214b1b3aec8a210be8e17f5
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-05-30 13:58:22 +0900

    update : コンテンツ作成。

--- src/components/section/Me/Me.svelte
+++ src/components/section/Me/Me.svelte
@@ -4,101 +4,41 @@
 <section class="section-me section-style-base">
   <div class="section-me-contents">
     <div class="perSection-title perSection-title-base">
-      <div class="blink-items">
-        <span>_</span><span>_</span><span>_</span><span>_</span><span>_</span>
-        <span>_</span><span>_</span><span>_</span><span>_</span><span>_</span>
-        <span>_</span>
-      </div>
-      について
+      <p class="name">上 原 龍 之 介</p>
     </div>
+    <p class="birth">二〇〇一年</p>
+    <p class="from">沖縄県宮古島市出身</p>
+    <p>東京都在住</p>
   </div>
-  <div class="about-me-block">
-    <p>ウエハラリュウノスケ。二〇〇一年</p>
-    <p>沖縄県宮古島市生まれ。東京都在住。</p>
-    <p>ウェブエンジニア。</p>
-    <p>ライブ。音楽鑑賞。ネットラジオ。料理。読書。</p>
-    <p>
-      ジャバスクリプト。タイプスクリプト。ノードジェイエス。リアクト。スベルト。ビュー。ネクスト。ワードプレス。ハブスポット。
-    </p>
-  </div>
+
+  <!-- 
+  <p>ウェブエンジニア。</p>
+  <p>ライブ。音楽。ネットラジオ。料理。読書。</p>
+  <p>
+    JavaScript。TypeScript。Node.js。React。Svelte。Vue。Next。CSSModules。TailWind。MatineUI。WordPress。Hubspot。
+  </p> -->
 </section>
 
 <style>
   .section-me {
     background-color: var(--turquoise-fresh);
-  }
-
-  .section-me-contents {
-    width: 1280px;
+    position: relative;
   }
 
   @media (max-width: 768px) {
-    .section-me-contents {
-      width: 100%;
-      padding-bottom: 2.5rem;
-    }
-
     .perSection-title {
       display: flex;
-      align-items: flex-end;
+      flex-direction: column;
+      padding-bottom: 1rem;
       gap: 10px;
     }
-
-    .blink-items {
-      position: relative;
-      top: 2px;
+    .name {
+      font-size: var(--font-size-lg);
     }
-
-    @keyframes blink {
-      0%,
-      100% {
-        opacity: 1;
-      }
-      50% {
-        opacity: 0;
-      }
+    .birth {
+      font-size: var(--font-size-sm);
     }
-
-    .blink-items span {
-      display: inline-block;
-      animation: blink 1s infinite;
-    }
-
-    .blink-items span:nth-child(1) {
-      animation-delay: 0s;
-    }
-    .blink-items span:nth-child(2) {
-      animation-delay: 0.2s;
-    }
-    .blink-items span:nth-child(3) {
-      animation-delay: 0.4s;
-    }
-    .blink-items span:nth-child(4) {
-      animation-delay: 0.1s;
-    }
-    .blink-items span:nth-child(5) {
-      animation-delay: 0.3s;
-    }
-    .blink-items span:nth-child(6) {
-      animation-delay: 0.5s;
-    }
-    .blink-items span:nth-child(7) {
-      animation-delay: 0.15s;
-    }
-    .blink-items span:nth-child(8) {
-      animation-delay: 0.35s;
-    }
-    .blink-items span:nth-child(9) {
-      animation-delay: 0.25s;
-    }
-    .blink-items span:nth-child(10) {
-      animation-delay: 0.05s;
-    }
-    .blink-items span:nth-child(11) {
-      animation-delay: 0.45s;
-    }
-    .blink-items span:nth-child(12) {
-      animation-delay: 0.3s;
+    .from {
     }
   }
 </style>
