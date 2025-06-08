4991aa25208584c9772b6ee73665cc6340f9af55
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-06-08 14:15:38 +0900

    update : リファクタ

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,17 +1,17 @@
 import type { Blog, Info } from "../types/microcms";
 
 export async function fetchCotos(): Promise<{ contents: Blog[] }> {
-  const res = await fetch(`${import.meta.env.VITE_MICROCMS_API_URL}/api/blogs`);
+  const res = await fetch(`${import.meta.env.VITE_MICROCMS_API_URL}api/blogs`);
   return await res.json();
 }
 
 export async function fetchInfos(): Promise<Info[]> {
-  const res = await fetch(`${import.meta.env.VITE_MICROCMS_API_URL}/api/info`);
+  const res = await fetch(`${import.meta.env.VITE_MICROCMS_API_URL}api/info`);
   const data = await res.json();
   return data.contents;
 }
 
 export async function fetchSpecificInfo(id: string): Promise<Info> {
-  const res = await fetch(`${import.meta.env.VITE_MICROCMS_API_URL}/api/info/${id}`);
+  const res = await fetch(`${import.meta.env.VITE_MICROCMS_API_URL}api/info/${id}`);
   return await res.json();
 }
